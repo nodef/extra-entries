c20c611612867d99b8f5ddbb025df68a06b6c58a
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 11:14:36 +0530

    :watermelon: cool fruit ;entries

--- src/entries.ts
+++ src/entries.ts
@@ -2,9 +2,9 @@ import type {Entries} from './_types';
 
 /**
  * Lists all key-value pairs.
- * @param x a map
+ * @param x entries
  */
-function* entries<T, U>(x: Entries<T, U>): IterableIterator<[T, U]> {
+function* entries<T, U>(x: Entries<T, U>): Entries<T, U> {
   yield* x;
 }
 export default entries;
