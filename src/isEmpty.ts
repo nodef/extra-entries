e65c7a1b3f5e5ee0aa251c29eea6b23da5ee997a
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 11:27:27 +0530

    :watermelon: cool fruit ;isEmpty

---
+++ src/isEmpty.ts
@@ -0,0 +1,12 @@
+import type {Entries} from './_types';
+
+/**
+ * Checks if entries is empty.
+ * @param x entries
+ */
+function isEmpty<T, U>(x: Entries<T, U>): boolean {
+  for(var _ of x)
+    return false;
+  return true;
+}
+export default isEmpty;
