6a3b3ea4a6f6a6cfbb08930e419414d1bd9b2a4d
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 08:56:45 +0530

    :watermelon: cool fruit ;union

---
+++ src/union.ts
@@ -0,0 +1,19 @@
+import id from './_id';
+import type {combineFn, Entries} from './_types';
+
+/**
+ * Gives entries present in any entries.
+ * @param x entries
+ * @param y another entries
+ * @param fn combine function (a, b)
+ */
+function union<T, U>(x: Entries<T, U>, y: Entries<T, U>, fn: combineFn<U>=null): Map<T, U> {
+  var fn = fn||id;
+  var a = new Map(x);
+  for(var [k, v] of y) {
+    if(!a.has(k)) a.set(k, v);
+    else a.set(k, fn(a.get(k), v));
+  }
+  return a;
+}
+export default union;
