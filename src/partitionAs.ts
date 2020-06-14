e0157b3c0c87abc14fe28aeb4ae5fda493af7f67
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 12:05:49 +0530

    :watermelon: cool fruit ;partitionAs

---
+++ src/partitionAs.ts
@@ -0,0 +1,20 @@
+import id from './_id';
+import type {mapFn, Entries} from './_types';
+
+/**
+ * Segregates values by similarity.
+ * @param x entries
+ * @param fn map function (v, k, x)
+ * @param ths this argument
+ */
+function partitionAs<T, U, V=U>(x: Entries<T, U>, fn: mapFn<T, U, U|V>, ths: object=null): Map<U|V, Map<T, U>> {
+  var fn = fn||id;
+  var a = new Map();
+  for(var [k, v] of x) {
+    var v1 = fn.call(ths, v, k, x);
+    if(!a.has(v1)) a.set(v1, new Map());
+    a.get(v1).set(k, v);
+  }
+  return a;
+}
+export default partitionAs;
