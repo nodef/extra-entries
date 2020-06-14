cffbf007ce1cd880c719336c1236d7ecfe015a47
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 11:59:49 +0530

    :watermelon: cool fruit ;head

--- src/head.ts
+++ src/head.ts
@@ -7,5 +7,6 @@ import type {Entries} from './_types';
 function head<T, U>(x: Entries<T, U>): [T, U] {
   for(var [k, v] of x)
     return [k, v];
+  return [undefined, undefined];
 }
 export default head;
