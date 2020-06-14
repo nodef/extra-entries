9085d1339c8287f1c58225a1f65161e47c489ecc
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 12:33:57 +0530

    :watermelon: cool fruit ;map

--- src/map.ts
+++ src/map.ts
@@ -1,4 +1,4 @@
-import type {mapFn} from './_types';
+import type {Entries, mapFn} from './_types';
 
 /**
  * Updates values based on map function.
@@ -6,7 +6,7 @@ import type {mapFn} from './_types';
  * @param fn map function (v, k, x)
  * @param ths this argument
  */
-function* map<K, V, W>(x: Iterable<[K, V]>, fn: mapFn<K, V, W>, ths: object=null): IterableIterator<[K, V]> {
+function* map<T, U, V=U>(x: Entries<T, U>, fn: mapFn<T, U, U|V>, ths: object=null): Entries<T, U|V> {
   for(var [k, v] of x)
     yield [k, fn.call(ths, v, k, x)];
 }
