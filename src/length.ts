70fa6db36b0206ba13297defec43a5f5b1600410
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 11:34:47 +0530

    :watermelon: cool fruit ;length

--- src/length.ts
+++ src/length.ts
@@ -1,10 +1,11 @@
 import size from './size';
+import type {Entries} from './_types';
 
 /**
  * Gets size of entries.
  * @param x entries
  */
-function length<K, V>(x: Iterable<[K, V]>): number {
+function length<T, U>(x: Entries<T, U>): number {
   return size(x);
 }
 export default length;
