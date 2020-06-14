2ec9039183964edce971aa3929c84dfebd0dab0d
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 08:41:12 +0530

    :watermelon: cool fruit ;values

--- src/values.ts
+++ src/values.ts
@@ -1,9 +1,11 @@
+import type {Entries} from './_types';
+
 /**
  * Lists all values.
  * @param x entries
  */
-function* values<K, V>(x: Iterable<[K, V]>): IterableIterator<V> {
-  for(var [, v] of x)
+function* values<T, U>(x: Entries<T, U>): IterableIterator<U> {
+  for(var [k, v] of x)
     yield v;
 }
 export default values;
