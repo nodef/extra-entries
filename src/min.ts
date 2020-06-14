2638c3fcbd0df0f48d4fcdb31ae9751e97fe1d39
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 12:04:04 +0530

    :watermelon: cool fruit ;min

---
+++ src/min.ts
@@ -0,0 +1,13 @@
+import range from './range';
+import type {compareFn, mapFn, Entries} from './_types';
+
+/**
+ * Finds smallest entry.
+ * @param x entries
+ * @param fc compare function (a, b)
+ * @param fm map function (v, k, x)
+ */
+function min<T, U, V=U>(x: Entries<T, U>, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): [T, U] {
+  return range(x, fc, fm)[0];
+}
+export default min;
