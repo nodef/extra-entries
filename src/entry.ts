4c0469b955ede5297ea3b555d5cf4e6fa7756353
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 11:18:42 +0530

    :watermelon: cool fruit ;entry

---
+++ src/entry.ts
@@ -0,0 +1,12 @@
+import {value} from 'extra-array';
+import type {Entries} from './_types';
+
+/**
+ * Picks an arbitrary entry.
+ * @param x entries
+ * @param r random seed 0->1
+ */
+function entry<T, U>(x: Entries<T, U>, r: number=Math.random()): [T, U] {
+  return value([...x], r);
+}
+export default entry;
