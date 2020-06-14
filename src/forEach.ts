48a1bca06acc00a9662e1e245917bfe50209d9f2
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 11:03:25 +0530

    :watermelon: cool fruit ;forEach

--- src/forEach.ts
+++ src/forEach.ts
@@ -1,12 +1,12 @@
-import type {calledFn} from './_types';
+import type {calledFn, Entries} from './_types';
 
 /**
- * Calls a function for each pair.
+ * Calls a function for each entry.
  * @param x entries
  * @param fn called function (v, k, x)
  * @param ths this argument
  */
-function forEach<K, V>(x: Iterable<[K, V]>, fn: calledFn<K, V>, ths: object=null): void {
+function forEach<T, U>(x: Entries<T, U>, fn: calledFn<T, U>, ths: object=null): void {
   for(var [k, v] of x)
     fn.call(ths, v, k, x);
 }
