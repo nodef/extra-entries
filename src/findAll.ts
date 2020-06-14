d9fa3f9717570c76f2ccd4aee0b2689cebfe933f
Subhajit Sahu <wolfram77@gmail.com>
2020-06-14 11:11:14 +0530

    :watermelon: cool fruit ;findAll

---
+++ src/findAll.ts
@@ -0,0 +1,13 @@
+import type {testFn, Entries} from './_types';
+
+/**
+ * Finds values passing the test.
+ * @param x entries
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
+function* findAll<T, U>(x: Entries<T, U>, fn: testFn<T, U>, ths: object=null): IterableIterator<U> {
+  for(var [k, v] of x)
+    if(fn.call(ths, v, k, x)) yield v;
+}
+export default findAll;
