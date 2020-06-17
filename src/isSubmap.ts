98afe0c349676002840afebffefe38a86c84b5f9
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 10:10:34 +0530

    :watermelon: cool fruit ;isSubmap

--- src/isSubmap.ts
+++ src/isSubmap.ts
@@ -1,5 +1,4 @@
-import id from './_id';
-import cmp from './_cmp';
+import {isSubmap as mapIsSubmap} from 'extra-map';
 import type {compareFn, mapFn, Entries} from './_types';
 
 /**
@@ -10,15 +9,6 @@ import type {compareFn, mapFn, Entries} from './_types';
  * @param fm map function (v, k, x)
  */
 function isSubmap<T, U, V=U>(x: Entries<T, U>, y: Entries<T, U>, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): boolean {
-  var fc = fc||cmp, fm = fm||id;
-  var x1 = new Map(x);
-  for(var [k, v] of y) {
-    if(!x1.has(k)) return false;
-    var u1 = fm(x1.get(k), k, x);
-    var v1 = fm(v, k, y);
-    if(fc(u1, v1)!==0) return false;
-  }
-  return true;
+  return mapIsSubmap(new Map(x), y, fc, fm);
 }
 export default isSubmap;
-// CYCLIC?
