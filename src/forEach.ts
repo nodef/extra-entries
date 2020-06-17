80ea3838979eb0acaed64da9504e45a76576473d
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 10:33:21 +0530

    :watermelon: cool fruit ;forEach

--- src/forEach.ts
+++ src/forEach.ts
@@ -1,13 +1,12 @@
+import {forEach as mapForEach} from 'extra-map';
 import type {calledFn, Entries} from './_types';
 
 /**
- * Calls a function for each entry.
+ * Calls a function for each value.
  * @param x entries
  * @param fn called function (v, k, x)
- * @param ths this argument
  */
-function forEach<T, U>(x: Entries<T, U>, fn: calledFn<T, U>, ths: object=null): void {
-  for(var [k, v] of x)
-    fn.call(ths, v, k, x);
+function forEach<T, U>(x: Entries<T, U>, fn: calledFn<T, U>): void {
+  mapForEach(x, fn);
 }
 export default forEach;
