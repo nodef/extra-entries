0ab76069099464b04ba4a3f5b91aff06d3384666
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 09:56:58 +0530

    :watermelon: cool fruit ;range

--- src/partitionAs.ts
+++ src/partitionAs.ts
@@ -1,20 +1,12 @@
-import id from './_id';
+import {partitionAs as mapPartitionAs} from 'extra-map';
 import type {mapFn, Entries} from './_types';
 
 /**
  * Segregates values by similarity.
  * @param x entries
  * @param fn map function (v, k, x)
- * @param ths this argument
  */
-function partitionAs<T, U, V=U>(x: Entries<T, U>, fn: mapFn<T, U, U|V>, ths: object=null): Map<U|V, Map<T, U>> {
-  var fn = fn||id;
-  var a = new Map();
-  for(var [k, v] of x) {
-    var v1 = fn.call(ths, v, k, x);
-    if(!a.has(v1)) a.set(v1, new Map());
-    a.get(v1).set(k, v);
-  }
-  return a;
+function partitionAs<T, U, V=U>(x: Entries<T, U>, fn: mapFn<T, U, U|V>): Map<U|V, Entries<T, U>> {
+  return mapPartitionAs(x, fn);
 }
 export default partitionAs;
--- src/range.ts
+++ src/range.ts
@@ -1,5 +1,4 @@
-import id from './_id';
-import cmp from './_cmp';
+import {range as mapRange} from 'extra-map';
 import type {compareFn, mapFn, Entries} from './_types';
 
 /**
@@ -10,16 +9,6 @@ import type {compareFn, mapFn, Entries} from './_types';
  * @returns [smallest, largest]
  */
 function range<T, U, V=U>(x: Entries<T, U>, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): [[T, U], [T, U]] {
-  var fc = fc||cmp, fm = fm||id;
-  var mk: T, mu: U, mv: U|V;
-  var nk: T, nu: U, nv: U|V;
-  var i = 0;
-  for(var [k, u] of x) {
-    var v = fm(u, k, x);
-    if(i===0 || fc(v, mv)<0) { mk = k; mu = u; mv = v; }
-    if(i===0 || fc(v, nv)>0) { nk = k; nu = u; nv = v; }
-    i++;
-  }
-  return [[mk, mu], [nk, nu]];
+  return mapRange(x, fc, fm);
 }
 export default range;
