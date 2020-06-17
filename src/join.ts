6487403a61df848df8ece3bfadc3769502346597
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 10:00:57 +0530

    :watermelon: cool fruit ;join

--- src/join.ts
+++ src/join.ts
@@ -1,11 +1,13 @@
-function join(ent, fmt='%k=%v', sep=',', idx=0, val=null, v0=Array.isArray(val)? val.length:0, bgn=0, end=Number.MAX_SAFE_INTEGER, z='') {
-  var i = -1;
-  for(var e of ent) {
-    if(++i<bgn) continue;
-    if(i>=end) break;
-    z += fmt.replace(/%i/g, idx++).replace(/%k/g, e[0]).replace(/%v/g, e[1])+sep;
-    if(val!=null) val[v0++] = e[1];
-  }
-  return z.substr(0, z.length-sep.length);
-};
-module.exports = join;
+import {join as mapJoin} from 'extra-map';
+import type {Entries} from './_types';
+
+/**
+ * Joins entries together.
+ * @param x entries
+ * @param sep separator (,)
+ * @param asc associator (=)
+ */
+function join<T, U>(x: Entries<T, U>, sep: string=',', asc: string='='): string {
+  return mapJoin(x, sep, asc);
+}
+export default join;
