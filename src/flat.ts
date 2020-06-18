adf9f7013b7545ef6a8fad5cef56e35255b53e00
Subhajit Sahu <wolfram77@gmail.com>
2020-06-18 12:52:44 +0530

    :watermelon: cool fruit ;flat

--- src/flat.ts
+++ src/flat.ts
@@ -1,14 +1,15 @@
 import is from './is';
 import {flat as mapFlat} from 'extra-map';
-import type {testFn, Entries} from './_types';
+import type {mapFn, testFn, Entries} from './_types';
 
 /**
  * Flattens nested entries to given depth.
  * @param x nested entries
  * @param n maximum depth (-1 => all)
+ * @param fm map function (v, k, x)
  * @param ft test function (v, k, x)
  */
-function flat<T>(x: Entries<T, any>, n: number=-1, ft: testFn<T, any>=null): Entries<T, any> {
-  return mapFlat(x, n, ft||is);
+function flat<T>(x: Entries<T, any>, n: number=-1, fm: mapFn<T, any, any>=null, ft: testFn<T, any>=null): Entries<T, any> {
+  return mapFlat(x, n, fm, ft||is);
 }
 export default flat;
